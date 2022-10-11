56036066ff84fb1a12886fd208883bae74e7f9fe
noro8514 <noro8514@colorado.edu>
2022-10-11 10:35:40 -0600

    ICA 8 Done

--- ica8/ica8.js
+++ ica8/ica8.js
@@ -1,2 +1,19 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello World!";
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+
+    if(mySrc === "../img/top-image.png"){
+        myImage.setAttribute("src", "../img/bottom-image.png");
+    }
+    else{
+        myImage.setAttribute("src","../img/top-image.png");
+    }
+};
+
+let bodyChange = document.querySelector("h1");
+bodyChange.addEventListener("mouseover", colorChange);
+
+function colorChange(){
+    bodyChange.style.color = "red";
+}
