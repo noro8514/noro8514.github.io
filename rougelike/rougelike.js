f930d6e72bdab5303bee95847ca7e1362389ab96
noro8514 <noro8514@colorado.edu>
2022-12-09 19:41:36 -0700

    It's Probably Done

--- rougelike/rougelike.js
+++ rougelike/rougelike.js
@@ -1,9 +1,24 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+let theScore = document.querySelector('.score');
+const play = document.querySelector('.play');
+const disclaimer = document.querySelector('.disclaimer');
+const instructions = document.querySelector('.instructions');
+
+theScore.textContent = "Score: " + 0;
 
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight - 50;
 
+ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+ctx.fillRect(0, 0, width, height);
+
+play.addEventListener("click",theGame,false);
+
+function theGame(){
+play.style.visibility = 'hidden';
+disclaimer.style.visibility = 'hidden';
+instructions.style.visibility = 'hidden';
 document.addEventListener("keydown",keyCheck,false);
 document.addEventListener("keydown",keyCheckProjectile,false);
 
@@ -153,6 +168,9 @@ class Player {
                         projectileArray[i].velY = 0;
                         projectileArray[i].deathCheck = true;
 
+                        scoreCount += 1;
+                        theScore.textContent = "Score: " + scoreCount;
+
                       }
                   }    
     }
@@ -211,14 +229,66 @@ class Player {
       
   }
 
+  class Door {
+    constructor(x, y, length, width) {
+      this.x = x;
+      this.y = y;
+      this.color = "white";
+      this.length = length;
+      this.width = width;
+
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.length);
+        ctx.fill();
+      }
+
+      update() {
+      }
+
+      collisionDetect(pX,pY) {
+        
+        if (pX + player.size >= this.x && pX + player.size <= this.x + this.width && pY + player.size >= this.y && pY + player.size <= this.y + this.length) {
+            if(this.width > this.length){
+                if(pY > height/2){
+                    player.y = 50;
+                }
+                else{
+                    player.y = height-70;
+                }
+            }
+            else{
+                if(pX > width/2){
+                    player.x = 50;
+                }
+                else{
+                    player.x = width - 50;
+                }
+            }
+            BGColor = `rgba(${random(0,50)},${random(0,50)},${random(0,50)},0.${random(1,5)})`;
+            //Here
+            makeEnemies();
+        }   
+    } 
+  }
+
   let projectileArray = [];
   let projectileCount = 0;
   let enemyArray = [];
   let difficulty = 10;
   let xVel = 0;
   let yVel = 0;
+  let BGColor = "rgba(0,0,0,0.25)";
+  let scoreCount = 0;
 
   const player = new Player(width/2,height/2,xVel,yVel,'rgb(255,0,0)',10);
+  const topDoor = new Door((width/2)-50, 20, 30, 100);
+  const bottomDoor = new Door((width/2)-50, height-40, 30, 100);
+  const leftDoor = new Door(0, (height/2)-50, 100, 30);
+  const rightDoor = new Door(width-30, (height/2)-50, 100, 30);
 
   for(let i = 0; i < difficulty; i++){
     let randomPosition = random(1,4);
@@ -239,11 +309,18 @@ class Player {
 
 
   function loop() {
-    ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
-    ctx.fillRect(0, 0, width, height);
+    drawBG(BGColor);
   
       player.draw();
       player.update(xVel,yVel);
+      topDoor.draw();
+      topDoor.collisionDetect(player.givePositionX(),player.givePositionY());
+      bottomDoor.draw();
+      bottomDoor.collisionDetect(player.givePositionX(),player.givePositionY());
+      leftDoor.draw();
+      leftDoor.collisionDetect(player.givePositionX(),player.givePositionY());
+      rightDoor.draw();
+      rightDoor.collisionDetect(player.givePositionX(),player.givePositionY());
       for(let i = 0; i < difficulty; i++){
         enemyArray[i].draw();
         enemyArray[i].update(player.givePositionX(),player.givePositionY());
@@ -261,6 +338,29 @@ class Player {
 
   
     requestAnimationFrame(loop);
+  }
+  function drawBG(theColor){
+    ctx.fillStyle = theColor;
+    ctx.fillRect(0, 0, width, height);
+  }
+
+  function makeEnemies(){
+    for(let i = 0; i < difficulty; i++){
+        let randomPosition = random(1,4);
+    
+        if(randomPosition == 1){
+        enemyArray[i] = new Enemy(random(0, width/4),random(0, height/4),0,0,'rgb(0,125,125)',random(5,10));}
+    
+        else if(randomPosition == 2){
+        enemyArray[i] = new Enemy(random(width-(width/4), width),random(0, height/4),0,0,'rgb(0,125,125)',random(5,10));}
+    
+        else if(randomPosition == 3){
+        enemyArray[i] = new Enemy(random(width-(width/4), width),random(height-(height/4), height),0,0,'rgb(0,125,125)',random(5,10));}
+    
+        else{
+        enemyArray[i] = new Enemy(random(0, width/4),random(height-(height/4), height),0,0,'rgb(0,125,125)',random(5,10));}
+    
+      }
   }
 
   function keyCheck(evt){
@@ -326,3 +426,4 @@ function sqrd(num){
 }
 
 loop();
+}
